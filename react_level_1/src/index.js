2c48388631b347272d72432c36cf3cf1ad96fa10
nudelx <alex.nudelman@samanage.com>
2017-12-25 14:16:25 +0200

    lesson 1

--- react_level_1/src/index.js
+++ react_level_1/src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 // import App from './App';
-import './index.css';
+import './index.css'
 // import PropTypes from 'prop-types';
 // import registerServiceWorker from './registerServiceWorker';
 //
 // ReactDOM.render(<MyComponent />, document.getElementById('root'));
 // registerServiceWorker();
 
-
 // var root = document.querySelector('#root')
 // console.log(root)
 // var el = document.createElement('div')
-// el.textContent = 'AAAAA'
+// el.textContent = 'Alex'
 // el.className = 'test'
 // root.appendChild(el)
 
@@ -24,4 +23,52 @@ import './index.css';
 // }
 //
 //
-// ReactDOM.render(<MyComponent children={'alex'}/>, document.getElementById('root'));
+
+NOT React
+var root = document.querySelector('#root')
+console.log(root)
+var el = document.createElement('div')
+el.textContent = 'Alex'
+el.className = 'test'
+root.appendChild(el)
+
+
+//REACT
+const AlexComponent = function({ children, msg }) {
+  return (
+    <div>
+      {children}
+      {msg ? <div>{msg}</div> : null}
+    </div>
+  )
+}
+ReactDOM.render(
+  <AlexComponent >
+    <div>
+      I am a kid<div>
+        I am a kid <div>I am a kid</div>
+      </div>
+    </div>
+  </AlexComponent>,
+  document.querySelector('#root')
+)
+
+// ReactDOM.render(React.createElement(
+//   AlexComponent,
+//   null,
+//   React.createElement(
+//     'div',
+//     null,
+//     'I am a kid',
+//     React.createElement(
+//       'div',
+//       null,
+//       'I am a kid ',
+//       React.createElement(
+//         'div',
+//         null,
+//         'I am a kid'
+//       )
+//     )
+//   )
+// ), document.querySelector('#root'));
