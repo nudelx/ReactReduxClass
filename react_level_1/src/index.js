62146179b24e788065731b0d319f3c9817ecccab
nudelx <alex.nudelman@samanage.com>
2017-12-28 12:00:03 +0200

    step 2

--- react_level_1/src/index.js
+++ react_level_1/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-// import App from './App';
+import App from './App';
 import './index.css'
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import registerServiceWorker from './registerServiceWorker';
 //
-// ReactDOM.render(<MyComponent />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
 // var root = document.querySelector('#root')
@@ -24,34 +24,70 @@ import './index.css'
 //
 //
 
-NOT React
-var root = document.querySelector('#root')
-console.log(root)
-var el = document.createElement('div')
-el.textContent = 'Alex'
-el.className = 'test'
-root.appendChild(el)
+// NOT React
 
 
-//REACT
-const AlexComponent = function({ children, msg }) {
-  return (
-    <div>
-      {children}
-      {msg ? <div>{msg}</div> : null}
-    </div>
-  )
-}
-ReactDOM.render(
-  <AlexComponent >
-    <div>
-      I am a kid<div>
-        I am a kid <div>I am a kid</div>
-      </div>
-    </div>
-  </AlexComponent>,
-  document.querySelector('#root')
-)
+// var createAlex = (msg, className , element) => {
+//   var root = element || document.querySelector('#root')
+//   console.log(root)
+//   var el = document.createElement('div')
+//   el.textContent = msg
+//   el.className = className
+//   root.appendChild(el)
+// }
+//
+//
+//  var el = document.querySelector('#root')
+//  var el = document.createElement('table')
+// createAlex('preved', 'test')
+
+// REACT
+// const AlexComponent = function({ msg, children, onClick}) {
+//   return (
+//     <div onClick={onClick}>
+//       {children}
+//       {(msg ? <div>{msg}</div> : null)}
+//     </div>
+//
+//   )
+// }
+//
+// class AAA extends Component {
+//
+//   state = { name: 'alex', time: null}
+//
+//   timerCallBack = () => {
+//     this.setState({ time: new Date().toLocaleTimeString()})
+//   }
+//
+//   componentDidMount() {
+//     // console.log(this)
+//     setInterval(this.timerCallBack,1000)
+//   }
+//
+//   clicked = () => {
+//     console.log(this)
+//     var { name } = this.state
+//     console.log(name)
+//   }
+//
+//   render() {
+//     console.log('render')
+//     return (<div><AlexComponent onClick={this.clicked} msg={`${this.state.name} hello ${this.state.time}`}/></div>)
+//   }
+// }
+//
+//
+//
+// ReactDOM.render(
+//
+//   <AAA></AAA>
+//   ,
+//   document.querySelector('#root')
+// )
+
+
+
 
 // ReactDOM.render(React.createElement(
 //   AlexComponent,
